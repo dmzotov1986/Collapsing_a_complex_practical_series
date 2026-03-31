3f61cd66b5d18182ff38677268508fd5da2124ea
dmzotov1986 <dmzotov1986@gmail.com>
2026-04-01 01:23:08 +0300

    update MathVisualizer.js

--- MathVisualizer.js
+++ MathVisualizer.js
@@ -5,13 +5,13 @@ x = []
 d = []
 e = []
 s_e = 0
-for(i = 0; i < b - a + 1; ++i) {
+for (i = 0; i < b - a + 1; ++i) {
 	x[i] = a / (b - i)
 	d[i] = c[i] * x[i]
 	c[i + 1] = c[i] - d[i]
-	s_e += e[b - a - i] = i == 0? 1: (i + a - 1) / i * e[b - a - i + 1]
+	s_e += e[b - a - i] = i == 0 ? 1 : (i + a - 1) / i * e[b - a - i + 1]
 }
-for(i = 0; i < b - a + 1; ++i) {
+for (i = 0; i < b - a + 1; ++i) {
 	e[i] *= c[0] / s_e
 }
 document.write("a: ", a, "<br>b: ", b, "<br>i: ", 0, "..", i - 1, "<br>c: ", c, "<br>x: ", x, "<br>d: ", d, "<br>e: ", e, "<br>s_e: ", s_e)
